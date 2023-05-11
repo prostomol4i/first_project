4fc01f84a625c4f6f88deb13c0b0a66964942a1a
Prostomol4i <prosto.mol4i.okk@gmail.com>
2023-05-12 00:03:31 +0600

    Создала MainScreen, сверстала основную ленту

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/common/constants/color_constants.dart';
 import 'package:flutter_application_1/src/router/router.dart';
-import 'package:flutter_application_1/src/screens/auth/auth_screen.dart';
+import 'package:flutter_application_1/src/screens/main/main_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
+      localizationsDelegates: [
+        DefaultCupertinoLocalizations.delegate,
+        DefaultMaterialLocalizations.delegate,
+      ],
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
-      home: AuthScreen(),
+      home: MainScreen(),
       theme: CupertinoThemeData(
         scaffoldBackgroundColor: AppColors.scaffoldBackground,
       ),
--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -2,22 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1/src/router/routing_const.dart';
 
 import '../screens/auth/auth_screen.dart';
-import '../screens/register_screen.dart';
+import '../screens/register/register_screen.dart';
 
 class AppRouter{
   static Route generateRoute(RouteSettings routeSettings){
      switch (routeSettings.name) {
       case AuthRoute:
         return CupertinoPageRoute(
-          builder: (context) => AuthScreen(),
+          builder: (context) => const AuthScreen(),
         );
       case RegisterRoute:
         return CupertinoPageRoute(
-          builder: (context) => RegisterScreen(),
+          builder: (context) => const RegisterScreen(),
         );
       default:
         return CupertinoPageRoute(
-          builder: (context) => AuthScreen(),
+          builder: (context) => const AuthScreen(),
         );
     }
   }
