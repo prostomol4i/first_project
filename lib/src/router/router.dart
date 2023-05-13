1177ab8297a591c109b693c9dd2f11c39a6b5821
Prostomol4i <prosto.mol4i.okk@gmail.com>
2023-05-14 02:48:30 +0600

    Сделала логику аутентификации

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1/src/router/routing_const.dart';
-
+import 'package:flutter_application_1/src/screens/main/main_screen.dart';
 import '../screens/auth/auth_screen.dart';
 import '../screens/register/register_screen.dart';
 
-class AppRouter{
-  static Route generateRoute(RouteSettings routeSettings){
-     switch (routeSettings.name) {
+class AppRouter {
+  static Route generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
       case AuthRoute:
         return CupertinoPageRoute(
           builder: (context) => const AuthScreen(),
@@ -15,6 +15,11 @@ class AppRouter{
         return CupertinoPageRoute(
           builder: (context) => const RegisterScreen(),
         );
+      case MainRoute:
+        return CupertinoPageRoute(
+          builder: (context) => const MainScreen(),
+        );
+
       default:
         return CupertinoPageRoute(
           builder: (context) => const AuthScreen(),
--- lib/src/router/routing_const.dart
+++ lib/src/router/routing_const.dart
@@ -1,6 +1,6 @@
-const String MainRoute = '/';
+const String MainRoute = '/main';
 
-const String AuthRoute = '/auth/login';
+const String AuthRoute = '/';
 const String RegisterRoute = '/auth/register';
 
 const String HomeRoute = '/restaurants';
