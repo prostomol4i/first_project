79ead7734c55cbbf682af9a397f7cce5156280ca
Prostomol4i <prosto.mol4i.okk@gmail.com>
2023-05-14 18:50:37 +0600

    Логика проверки авторизации пользователя закончена

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,43 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/common/constants/color_constants.dart';
 import 'package:flutter_application_1/src/router/router.dart';
 import 'package:flutter_application_1/src/router/routing_const.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('tokens');
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String initialRoute = AuthRoute;
+  @override
+  void initState() {
+    Box tokensBox = Hive.box('tokens');
+    if (tokensBox.get('access') != null || tokensBox.get('refresh') != null) {
+      initialRoute = MainRoute;
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      localizationsDelegates: [
+    return CupertinoApp(
+      localizationsDelegates: const [
         DefaultCupertinoLocalizations.delegate,
         DefaultMaterialLocalizations.delegate,
       ],
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
-      initialRoute: AuthRoute,
-      theme: CupertinoThemeData(
+      initialRoute: initialRoute,
+      theme: const CupertinoThemeData(
         scaffoldBackgroundColor: AppColors.scaffoldBackground,
       ),
     );
--- lib/src/screens/main/main_screen.dart
+++ lib/src/screens/main/main_screen.dart
@@ -9,33 +9,38 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.map)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.heart)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person)),
-        ],
-      ),
-      tabBuilder: (context, index) {
-        return CupertinoTabView(
-          builder: (context) {
-            switch (index) {
-              case 0:
-                return const HomeScreen();
-              case 1:
-                return const RegisterScreen();
-              case 2:
-                return const AuthScreen();
-              case 3:
-                return const ProfileScreen();
-              default:
-                return const HomeScreen();
-            }
-          },
-        );
+    return WillPopScope(
+      onWillPop: () {
+        return Future.value(false);
       },
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.map)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.heart)),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.person)),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(
+            builder: (context) {
+              switch (index) {
+                case 0:
+                  return const HomeScreen();
+                case 1:
+                  return const RegisterScreen();
+                case 2:
+                  return const AuthScreen();
+                case 3:
+                  return const ProfileScreen();
+                default:
+                  return const HomeScreen();
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
